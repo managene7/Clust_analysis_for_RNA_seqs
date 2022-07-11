62273f5cf08b95617d289f05bc6b333548c1ef12
Minkyu Park <minkju3003@gmail.com>
2022-07-11 01:45:40 -0400

    third commit

--- RNA_seq_tool-clust_result_processor_v2.0_py3.py
+++ RNA_seq_tool-clust_result_processor_v2.0_py3.py
@@ -11,7 +11,7 @@ import sys
 
 args = sys.argv[1:]
 
-option_dict={'-out':"",'-dot_size':"1.0",'-rep':"",'-log2':'no','-fig_w':"10", "-fig_h":"20",'-dpi':"300"}
+option_dict={'-out':"",'-dot_size':"1.0",'-rep':"",'-log2':'no','-fig_w':"10", "-fig_h":"20",'-dpi':"300",'-clust':"Clusters_Objects.tsv", '-min':'0'}
 for i in range(len(args)):
     if args[i].startswith("-"):
         try:
@@ -25,7 +25,7 @@ Usage;
 
 This code is for analysis of RNA-seq and data visualization
 -def    cl: generate individual files for cluster in teh clust result.
-            -clust      file name of cluster info by clust analysis
+            -clust      file name of cluster info by clust analysis (default is 'Clusters_Objects.tsv')
             -dge        file name of digital gene expression
             -rep        file name of replicate info if applicable (option)
             -log2       yes => convert dge into log2 value, no => do not convert (default is no)
@@ -51,6 +51,12 @@ This code is for analysis of RNA-seq and data visualization
             -fig_h      height of the figure (default is 20)
             -dot_size   size of dots in the plot (default is 1.0)
             -dpi        resolution of figure (default is 300)
+
+        convert: convert replicates into single values by average (and log2 value). 
+            -dge        file name of digital gene expression
+            -rep        file name of replicate info (optional)
+            -log2       yes => convert dge into log2 value, no => do not convert (default is no)
+            -min        minimum value threshold of dges in each gene (default is 0)
 ___________________________________________________________________________________________
 """)
                 quit()
@@ -66,12 +72,11 @@ import csv
 #dot_size=float(option_dict['-dot_size'])
 #__________convert clust result into individual cluster files_____________________
 
-def clust_divider(cl_file, dge_file, rep_file,log2):
+def clust_divider(cl_file):
     print ("Dividing clust into separate files..\n")
     cl_name=cl_file
     cl=open(cl_name,'r')
-    data_name=dge_file
-
+    
     #__clust info parsing__
     clust_dic={}
     cl_lines=cl.readlines()
@@ -95,32 +100,44 @@ def clust_divider(cl_file, dge_file, rep_file,log2):
                         clust_dic[cl_names[i]].append(row[i])
                 except:
                     pass
+    return clust_dic
+
+def Parsing_dge_data(dge_file, log2, cl_write):
+    data_dic={}
+    name_list=[]
+    name_list.append("head")
+    #minimum_value=float(min)
     #__parsing dge data__
-    data_file=open(data_name,'r')
+    data_file=open(dge_file,'r')
     line=data_file.readline().split()
     new_first_line=(',').join(line) # generate head line
+    data_dic['head']=new_first_line
     #__dge info parsing__
-    data_dic={}
     while 1:
         line=data_file.readline().strip()
         if line=="":
             break
         line_list=line.split()
+        #__filtering_with_min_value__
         if log2=="yes":
-            #print ("Convert dge into log2 value..\n")
-            new_line_list=[line_list[0]]
-            temp=list(map(lambda x: str(math.log(float(x)+1,2)), line_list[1:]))
-            line_list=new_line_list+temp
+            if cl_write=="yes":
+                new_line_list=[line_list[0]]
+                temp=list(map(lambda x: str(round(math.log(float(x)+1,2),2)), line_list[1:]))
+                line_list=new_line_list+temp
 
         new_line=(",").join(line_list)
-        data_dic[line_list[0]]=new_line
+        key=line_list[0]
+        data_dic[key]=new_line
+        name_list.append(key)
+    return data_dic, name_list
 
-    #__replicate info parsing__
+def process_dge_data(rep_file, data_dic, log2, min, cl_write):#__replicate info parsing__
+    minimum_value=float(min)
+    print ("Calculate average dge of replicates..\n")
     if rep_file!="":
-        print ("Calculate average dge of replicates..\n")
         rep_open=open(rep_file, 'r')
         rep_read=rep_open.readlines()
-        sample_id_list=new_first_line.split(",")
+        sample_id_list=data_dic['head'].split(",")
 
         rep_cont=[]
         for line in rep_read:
@@ -129,29 +146,49 @@ def clust_divider(cl_file, dge_file, rep_file,log2):
             index=[]
             for rep in rep_list:
                 index.append(sample_id_list.index(rep))
-            rep_cont.append([rep_list[0]+"_avg", index])
+            if log2!="yes":
+                rep_cont.append([rep_list[0]+"_avg", index])
+            else:
+                rep_cont.append([rep_list[0]+"_log2_avg", index])
         
-        #__convert dge info to average value based on replicates__
-        new_data_dic={}
-        for key, cont in data_dic.items():
+    #__convert dge info to average value based on replicates__
+    new_data_dic={}
+    for key, cont in data_dic.items():
+        new_first_line=data_dic['head']
+        new_first_line=[new_first_line.split(",")[0]] # generate new head line
+        if key !="head":
             cont=cont.split(",")
             new_cont=[cont[0]]
-            new_first_line=[new_first_line.split(",")[0]] # generate new head line
-            for rep in rep_cont:
-                new_first_line.append(rep[0])
-                value=0
-                for ind in rep[1]:
-                    value=value+float(cont[ind])
-                value=value/float(len(rep[1]))
-                if log2=="yes":
-                    value=math.log(value+1,2)
-                new_cont.append(str(value))
-            new_cont=",".join(new_cont)
-            new_first_line=",".join(new_first_line)
-            new_data_dic[key]=new_cont
-        
-        data_dic=new_data_dic
+            if rep_file!="":
+                for rep in rep_cont:
+                    new_first_line.append(rep[0])
+                    value=0
+                    #print (cont)
+                    for ind in rep[1]:
+                        value=value+float(cont[ind])
+                    value=value/float(len(rep[1]))
+                    new_cont.append(value)
+                
+                new_first_line=",".join(new_first_line)
+                new_data_dic['head']=new_first_line
+
+            else:
+                new_data_dic["head"]=data_dic['head']
+                new_cont=[cont[0]]+list(map(lambda x: float(x), cont[1:]))
+            
+            if max(new_cont[1:])>=minimum_value:
+                if log2=="yes" and cl_write!="yes":
+                    new_cont=[new_cont[0]]+list(map(lambda x: str(round(math.log(x+1,2),2)), new_cont[1:]))
+                else:
+                    new_cont=[new_cont[0]]+list(map(lambda x: str(round(x,2)), new_cont[1:]))
+                new_cont=",".join(new_cont)
+                new_data_dic[key]=new_cont
+            
     
+    data_dic=new_data_dic
+    return data_dic
+
+def generate_clust_files(clust_dic, data_dic, log2):
     #__generate clust files with dge data__
     for cl, g_names in list(clust_dic.items()):
         cl_list=cl.split()
@@ -160,6 +197,7 @@ def clust_divider(cl_file, dge_file, rep_file,log2):
             out_file=open(cl_concatenated+"_log2.csv",'w')
         else:
             out_file=open(cl_concatenated+".csv",'w')
+        new_first_line=data_dic['head']
         out_file.write(new_first_line+'\n')
         for g in g_names:
             #print g
@@ -169,6 +207,15 @@ def clust_divider(cl_file, dge_file, rep_file,log2):
                 print(g)
     print("Completed!!")
             
+def write_converted_data(dge_file_name, data_dic_converted, gene_list, rep):
+    if rep !="":
+        out_file=open(dge_file_name+"_converted_avg.txt",'w')
+    else:
+        out_file=open(dge_file_name+"_converted.txt",'w')
+    for gene in gene_list:
+        if gene in data_dic_converted:
+            out_file.write("\t".join(data_dic_converted[gene].split(','))+"\n")
+
 
 #___________draw boxplot________________________________    
 
@@ -218,8 +265,6 @@ def violin_plotting(consensus, w, h, dot, dpi):
         plt.savefig(name[:-4]+'_violin_plot.jpg',dpi=dpi_value, figsize=(width,height))
         plt.close()
 
-
-
 #_____________draw clusterd heat map______________________     
 
 def heatmap_plotting(def_option, w, h, consensus, dpi):
@@ -249,7 +294,19 @@ def heatmap_plotting(def_option, w, h, consensus, dpi):
 
 def main():
     if option_dict['-def']=="cl":
-        clust_divider(option_dict['-clust'], option_dict['-dge'], option_dict['-rep'], option_dict['-log2'])
+        clust_dic=clust_divider(option_dict['-clust'])
+        data_dic=Parsing_dge_data(option_dict['-dge'], option_dict['-log2'], "yes")[0]
+        if option_dict['-rep']!="":
+            data_dic=process_dge_data(option_dict['-rep'], data_dic, option_dict['-log2'],0,"yes")
+        generate_clust_files(clust_dic, data_dic, option_dict['-log2'])
+
+    elif option_dict['-def']=="convert":
+        data_dic_tuple=Parsing_dge_data(option_dict['-dge'], option_dict['-log2'], "no")
+        data_dic=data_dic_tuple[0]
+        gene_list=data_dic_tuple[1]
+        data_dic_processed=process_dge_data(option_dict['-rep'], data_dic, option_dict['-log2'], option_dict['-min'],"no")
+        write_converted_data(option_dict['-dge'], data_dic_processed, gene_list, option_dict["-rep"])
+
     elif option_dict['-def']=="hmap" or option_dict['-def']=="hmap_fc" :
         heatmap_plotting(option_dict['-def'], option_dict['-fig_w'], option_dict['-fig_h'], option_dict['-consensus'], option_dict['-dpi'])
     elif option_dict['-def']=="bp":
